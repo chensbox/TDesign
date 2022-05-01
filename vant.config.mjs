7e80666325b80febf4e11c349870b6e86264459f
chenjiajian <770230504@qq.com>
2022-05-02 00:40:51 +0800

    初始提交

---
+++ vant.config.mjs
@@ -0,0 +1,39 @@
+export default {
+  name: 'TDesign',
+  build: {
+    css: {
+      preprocessor: 'sass',
+    },
+    site: {
+      publicPath: '/TDesign/',
+    },
+  },
+  site: {
+    title: 'TDesign',
+    logo: 'https://img.yzcdn.cn/vant/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
