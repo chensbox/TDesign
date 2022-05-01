be0f1b4222bb92582a35232db6977515c92c89f7
chenjiajian <770230504@qq.com>
2022-05-02 01:52:34 +0800

    ''

---
+++ docs/quickstart.md
@@ -0,0 +1,14 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i TDesign
+
+# 通过 yarn
+yarn add TDesign
+
+# 通过 pnpm
+pnpm add TDesign
+```
---
+++ src/demo-button/demo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <demo-block title="基础用法">
+    <demo-button type="primary" style="margin-left: 15px">按钮</demo-button>
+  </demo-block>
+
+  <demo-block title="自定义颜色">
+    <demo-button color="#03a9f4" style="margin-left: 15px">按钮</demo-button>
+  </demo-block>
+</template>
+
+<script>
+import demoButton from '../index.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: { demoButton },
+});
+</script>
