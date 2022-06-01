8f1031d8e4f4a7190370bc2cca3850df03563717
chenjiajian <770230504@qq.com>
2022-06-01 12:00:08 +0800

    popup

--- src/popup/demo/index.vue
+++ src/popup/demo/index.vue
@@ -1,26 +1,75 @@
 <template>
   <demo-block title="基础用法">
-    <t-button @click="onclick">点击</t-button>
+    <cell-gropu>
+      <cell title="展示弹出层" @click="show = true"></cell>
+    </cell-gropu>
+    <popup v-model="show">内容</popup>
+  </demo-block>
 
-    <popup v-model="show"></popup>
+  <demo-block title="弹出位置">
+    <cell-gropu>
+      <cell title="底部弹出" @click="onclick('bottom')"></cell>
+      <cell title="顶部弹出" @click="onclick('top')"></cell>
+      <cell title="左侧弹出" @click="onclick('left')"></cell>
+      <cell title="右侧弹出" @click="onclick('right')"></cell>
+    </cell-gropu>
+    <popup v-model="bottom" position="bottom"></popup>
+    <popup v-model="top" position="top"></popup>
+    <popup v-model="left" position="left"></popup>
+    <popup v-model="right" position="right"></popup>
+  </demo-block>
+
+  <demo-block title="圆角弹窗">
+    <cell-gropu>
+      <cell title="底部弹出" @click="round = true"> </cell>
+    </cell-gropu>
+    <popup v-model="round" round position="bottom" :style="{ height: '30%' }">
+    </popup>
+  </demo-block>
+
+  <demo-block title="关闭图标">
+    <cell-gropu>
+      <cell title="关闭图标" @click="closeable = true"> </cell>
+      <cell title="图标位置" @click="closeablePositon = true"> </cell>
+    </cell-gropu>
+    <popup
+      v-model="closeable"
+      position="bottom"
+      :style="{ height: '30%' }"
+      closeable
+    />
+    <popup
+      v-model="closeablePositon"
+      position="bottom"
+      :style="{ height: '30%' }"
+      closeable
+      close-icon-position="top-left"
+    />
   </demo-block>
 </template>
 
 <script>
+import cell from '../../cell/index.vue'
+import cellGropu from '../../cell-group/index.vue'
 import TButton from '../../button/index.vue'
 import popup from '../index.vue'
 export default {
-  components: { popup, TButton },
+  components: { popup, TButton, cell, cellGropu },
   data() {
     return {
-      show: false
+      show: false,
+      left: false,
+      right: false,
+      top: false,
+      bottom: false,
+      round: false,
+      closeable: false,
+      closeablePositon: false
     }
   },
   methods: {
-    onclick() {
-      console.log('1')
-      this.show = !this.show
-      console.warn(this.show)
+    onclick(position) {
+      this[position] = true
     }
   }
 }
--- src/popup/index.vue
+++ src/popup/index.vue
@@ -1,46 +1,144 @@
 <template>
-  <transition name="slide-fade">
-    <div
-      class="popup"
-      v-show="modelValue"
-      @click="$emit('update:modelValue', false)"
-    >
-      <overlay
-        :show="modelValue"
-        @click="$emit('update:modelValue', false)"
-      ></overlay>
+  <transition :name="animation">
+    <div class="popup" v-show="modelValue" :style="style">
+      <icon
+        name="close"
+        class="close-icon"
+        :style="iconPositon"
+        size="20px"
+        v-if="closeable"
+        @click="onclick"
+      />
+      <slot />
+      <overlay :show="modelValue" @click="onclick" />
     </div>
   </transition>
 </template>
 
 <script>
+import { reactive, ref } from '@vue/reactivity'
 import overlay from '../overlay/index.vue'
-export default {
-  components: { overlay },
-  props: {
-    modelValue: Boolean
+import icon from '../icon/index.vue'
+const components = { overlay, icon }
+const props = {
+  modelValue: Boolean,
+  position: String,
+  round: Boolean,
+  closeable: Boolean,
+  closeIconPosition: { type: String, default: 'top-right' }
+}
+const emits = ['close']
+//top bottom right left
+const setup = (props, { slots, emit }) => {
+  const animation = ref('slide-fade-bottom')
+  const style = reactive({})
+  const iconPositon = reactive({})
+  if (!props.position) {
+    animation.value = 'fade-in'
+    style.left = '50%'
+    style.top = '50%'
+    style.transform = 'translate(-50%, -50%)'
   }
+  console.log(props.closeIconPosition, '????????????')
+  if (props.position == 'bottom') {
+    style.left = '0px'
+    style.bottom = '0px'
+    style.width = '100%'
+    animation.value = 'slide-fade-bottom'
+  }
+  if (props.position == 'top') {
+    style.left = '0px'
+    style.top = '0px'
+    style.width = '100%'
+    animation.value = 'slide-fade-top'
+  }
+  if (props.position == 'left') {
+    style.left = '0px'
+    style.top = '0px'
+    style.height = '100%'
+    animation.value = 'slide-fade-left'
+  }
+  if (props.position == 'right') {
+    style.right = '0px'
+    style.top = '0px'
+    style.height = '100%'
+    animation.value = 'slide-fade-right'
+  }
+  if (props.round) {
+    style['border-radius'] = '16px 16px 0 0 '
+  }
+
+  if (props.closeable) {
+    iconPositon.color = '#c8c9cc'
+    iconPositon['font-weight'] = 'bold'
+
+    if (props.closeIconPosition) {
+      const [ordinate, abscissa] = props.closeIconPosition.split('-')
+      iconPositon[ordinate] = '10px'
+      iconPositon[abscissa] = '15px'
+    }
+  }
+
+  const onclick = event => {
+    emit('update:modelValue', false)
+  }
+  return { style, animation, iconPositon, onclick }
+}
+export default {
+  components,
+  props,
+  setup
 }
 </script>
 
 <style lang="less" scoped>
 .popup {
   position: fixed;
-  left: 0;
-  bottom: 0;
-  z-index: 10;
-  width: 100%;
-  height: 30%;
+  z-index: 100;
+  min-width: 150px;
+  min-height: 120px;
   background: #fff;
 }
 
-.slide-fade-enter-active,
-.slide-fade-leave-active {
+.close-icon {
+  position: absolute;
+  cursor: pointer;
+  color: '#c8c9cc' !important;
+}
+
+.fade-in-enter-active,
+.fade-in-leave-active,
+.slide-fade-bottom-enter-active,
+.slide-fade-bottom-leave-active,
+.slide-fade-top-enter-active,
+.slide-fade-top-leave-active,
+.slide-fade-left-enter-active,
+.slide-fade-left-leave-active,
+.slide-fade-right-enter-active,
+.slide-fade-right-leave-active {
   transition: all 0.25s ease-out;
 }
 
-.slide-fade-enter-from,
-.slide-fade-leave-to {
+.slide-fade-bottom-enter-from,
+.slide-fade-bottom-leave-to {
   transform: translateY(100%);
+}
+
+.slide-fade-top-enter-from,
+.slide-fade-top-leave-to {
+  transform: translateY(-100%);
+}
+.slide-fade-left-enter-from,
+.slide-fade-left-leave-to {
+  transform: translateX(-100%);
+}
+.slide-fade-right-enter-from,
+.slide-fade-right-leave-to {
+  transform: translateX(100%);
+}
+
+.fade-in-enter-from,
+.fade-in-leave-to {
+  opacity: 1;
 }
 </style>
