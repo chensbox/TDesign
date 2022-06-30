eab1249f5ace05dc318d0a674c0c0804c45aaaf0
chenjiajian <770230504@qq.com>
2022-07-01 00:59:22 +0800

    signature

--- src/signature/demo/index.vue
+++ src/signature/demo/index.vue
@@ -1,9 +1,33 @@
 <template>
   <demo-block title="基础用法">
-    <signature></signature>
+    <signature :line-width="width" :color="color"></signature>
+    <picker :colums="colum" title="设置画笔" @change="onChange"></picker>
   </demo-block>
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity'
 import signature from '../index.vue'
+import picker from '../../picker/index.vue'
+const width = ref(5)
+const color = ref('blue')
+
+const colum = [
+  // 第一列
+  {
+    values: ['2', '4', '6', '8', '10'],
+    defaultIndex: 2
+  },
+  // 第二列
+  {
+    values: ['red', 'blue', 'black'],
+    defaultIndex: 1
+  }
+]
+
+const onChange = e => {
+  console.log(e)
+  width.value = e[0]
+  color.value = e[1]
+}
 </script>
--- src/signature/index.vue
+++ src/signature/index.vue
@@ -1,28 +1,54 @@
 <template>
   <div class="signature">
-    <canvas id="canvas" :width="width" :height="height"></canvas>
+    <div class="signature-canvas">
+      <canvas
+        id="myCanvas"
+        :width="width"
+        :height="height"
+        @touchstart="touchstart"
+        @touchmove="touchmove"
+        @touchend="touchend"
+      ></canvas>
+    </div>
+    <div class="signature-control-bar">
+      <t-button class="signature-button" type="wanner" round @click="reset"
+        >重置</t-button
+      >
+      <t-button class="signature-button" round>确定</t-button>
+    </div>
   </div>
 </template>
 
 <script>
+import { onMounted } from '@vue/runtime-core'
 import TButton from '../button/index.vue'
 const name = 'signature'
 const components = { TButton }
 const props = {
   width: {
     type: [String, Number],
-    default: document.body.clientWidth
+    default: document.body.clientWidth - 2
   },
   height: {
     type: [String, Number],
     default: 300
+  },
+  lineWidth: {
+    type: [String, Number],
+    default: 2
+  },
+  color: {
+    type: String,
+    default: 'black'
   }
 }
 
 function setup(props) {
   let isMouseMove = false,
+    canvas,
     widthVal = 2,
     colorVal = 'pink',
+    ctx,
     lastX,
     lastY
 
@@ -30,8 +56,8 @@ function setup(props) {
   const drawLine = (x, y, isT) => {
     if (isT) {
       ctx.beginPath()
-      ctx.lineWidth = widthVal //设置线宽状态
-      ctx.strokeStyle = colorVal //设置线的颜色状态
+      ctx.lineWidth = props.lineWidth //设置线宽状态
+      ctx.strokeStyle = props.color //设置线的颜色状态
       ctx.lineCap = 'round'
       ctx.lineJoin = 'round'
       ctx.moveTo(lastX, lastY)
@@ -43,6 +69,40 @@ function setup(props) {
     lastX = x
     lastY = y
   }
+
+  const touchstart = e => {
+    document.body.classList.add('dis-scroll')
+    isMouseMove = true
+    drawLine(
+      e.touches[0].pageX - canvas.offsetLeft,
+      e.touches[0].pageY - canvas.offsetTop,
+      false
+    )
+  }
+  const touchmove = e => {
+    if (isMouseMove || 'ontouchstart' in document.body) {
+      drawLine(
+        e.touches[0].pageX - canvas.offsetLeft,
+        e.touches[0].pageY - canvas.offsetTop,
+        true
+      )
+    }
+  }
+  const touchend = e => {
+    isMouseMove = false
+    document.body.classList.remove('dis-scroll')
+  }
+  //清空画图
+  function reset() {
+    ctx.beginPath()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.closePath() //可加入，可不加入
+  }
+  onMounted(() => {
+    canvas = document.querySelector('#myCanvas')
+    ctx = canvas.getContext('2d')
+  })
+  return { touchstart, touchmove, touchend, reset }
 }
 export default {
   name,
@@ -52,9 +112,23 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .signature {
-  border: 1px dashed gray;
+  // border: 1px dashed gray;
   // border: 1px solid gray;
+  &-canvas {
+    background: rgb(255, 255, 255);
+    border: 1px dashed gray;
+  }
+  &-control-bar {
+    display: flex;
+    justify-content: end;
+  }
+  &-button {
+    margin: 10px 5px;
+  }
+}
+.dis-scroll {
+  overflow: hidden !important;
 }
 </style>
