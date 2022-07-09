4986a00bcf1dcbbccd749efb5f8073c143faac12
chenjiajian <770230504@qq.com>
2022-07-09 19:04:50 +0800

    zancun

--- src/pull-refresh/demo/index.vue
+++ src/pull-refresh/demo/index.vue
@@ -1,9 +1,19 @@
 <template>
   <demo-block title="基础用法">
+    <tabs v-model="active">
+      <tab title="dfd">df</tab>
+      <tab title="dfd">df</tab>
+      <tab title="dfd">df</tab>
+    </tabs>
     <pull-refresh></pull-refresh>
   </demo-block>
 </template>
 
 <script setup>
+import tabs from '../../tabs/index.vue'
+import tab from '../../tab/index.vue'
 import PullRefresh from '../index.vue'
+import { ref } from '@vue/reactivity'
+console.log(tab, tabs)
+const active = ref(0)
 </script>
