e229d8ba9ed9aa270a6283bfaefe7b6264ab4ddf
chenjiajian <770230504@qq.com>
2022-07-10 00:39:55 +0800

    下拉刷新

--- src/pull-refresh/demo/index.vue
+++ src/pull-refresh/demo/index.vue
@@ -1,19 +1,68 @@
 <template>
-  <demo-block title="基础用法">
-    <tabs v-model="active">
-      <tab title="dfd">df</tab>
-      <tab title="dfd">df</tab>
-      <tab title="dfd">df</tab>
-    </tabs>
-    <pull-refresh></pull-refresh>
-  </demo-block>
+  <tabs v-model="active" line-animation>
+    <tab title="基础用法">
+      <pull-refresh v-model="isLoading" class="demo-pull" @refresh="refresh">
+        <p>下拉刷新次数:{{ count }}</p>
+      </pull-refresh>
+    </tab>
+    <tab title="成功提示">
+      <pull-refresh class="demo-pull" v-model="isLoading" @refresh="onRefresh">
+        <p>下拉刷新次数:{{ count2 }}</p>
+      </pull-refresh>
+    </tab>
+    <tab title="自定义提示">
+      <pull-refresh class="demo-pull">
+        <p>下拉试试</p>
+      </pull-refresh>
+    </tab>
+  </tabs>
 </template>
 
 <script setup>
 import tabs from '../../tabs/index.vue'
 import tab from '../../tab/index.vue'
 import PullRefresh from '../index.vue'
+import { Toast } from '../../toast/index'
 import { ref } from '@vue/reactivity'
-console.log(tab, tabs)
+import { sleep } from '../../utils'
 const active = ref(0)
+const count = ref(0)
+const count2 = ref(0)
+const isLoading = ref(false)
+const refresh = done => {
+  sleep(2000).then(() => {
+    console.log('done')
+    done()
+    count.value++
+
+    isLoading.value = false
+  })
+}
+const onRefresh = function (done) {
+  sleep(2000).then(() => {
+    done()
+    isLoading.value = false
+    count2.value++
+    Toast('刷新成功')
+  })
+}
 </script>
+
+<style lang="less">
+.demo-pull-refresh {
+  padding-bottom: 0;
+}
+.demo-pull {
+  height: calc(100vh - 95px);
+}
+.tabs-body {
+  padding: 0;
+}
+
+.demo-pull-refresh {
+  p {
+    line-height: 50px;
+    margin-left: 20px;
+  }
+}
+</style>
--- src/pull-refresh/index.vue
+++ src/pull-refresh/index.vue
@@ -5,28 +5,104 @@
     @touchmove="touchmove"
     @touchend="touchend"
   >
-    <slot></slot>
+    <div class="pull-refresh-track" :style="trackStyle">
+      <div class="pull-refresh-track-header">
+        <span v-if="!modelValue">{{
+          offsetY > 50 ? '释放即可刷新...' : '下拉即可刷新...'
+        }}</span>
+        <span v-else><icon name="loading" /> 加载中...</span>
+      </div>
+
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
+import Icon from '../icon/index.vue'
+
+import { ref } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
+import { sleep } from '../utils'
 const name = 'pull-refresh'
-const props = {}
-function setup(props, context) {
-  const touchstart = e => {
-    console.log(touchstart)
-  }
+const emits = ['update:modelValue', 'refresh']
+
+const props = {
+  modelValue: Boolean
+}
+const components = { Icon }
+function setup(props, { emit }) {
+  const offsetY = ref(0)
+  const duration = ref(0)
+  const trackStyle = computed(() => {
+    return {
+      transform: `translateY(${offsetY.value}px)`,
+      transition: `all ${duration.value}s`
+    }
+  })
+  let curY = 0,
+    startY,
+    moveY
+  const touchstart = e => (startY = e.touches[0].pageY)
+  const done = () => (offsetY.value = 0)
   const touchmove = e => {
-    console.log(touchmove)
+    duration.value = 0
+    moveY = e.touches[0].pageY
+    if (moveY - startY < 0 || props.modelValue) {
+      return
+    }
+    const distance = moveY - startY
+    offsetY.value = distance
+    console.log(moveY)
   }
-  const touchend = e => {
-    console.log(touchend)
+  const touchend = async e => {
+    if (props.modelValue) {
+      return
+    }
+
+    if (offsetY.value < 50) {
+      duration.value = 0.2
+      return done()
+    }
+    duration.value = 0.2
+    offsetY.value = 50
+    emit('update:modelValue', true)
+    emit('refresh', done)
   }
-  return { touchstart, touchmove, touchend }
+  return { touchstart, touchmove, touchend, trackStyle, offsetY }
 }
 export default {
   name,
+  components,
   props,
+  emits,
   setup
 }
 </script>
+
+<style lang="less">
+.pull-refresh {
+  overflow: hidden;
+  &-track {
+    position: relative;
+    height: 100%;
+    &-header {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 50px;
+      overflow: hidden;
+      color: #969799;
+      font-size: 14px;
+      line-height: 50px;
+      text-align: center;
+      -webkit-transform: translateY(-100%);
+      transform: translateY(-100%);
+    }
+    .icon-loading {
+      margin-bottom: 3px;
+    }
+  }
+}
+</style>
