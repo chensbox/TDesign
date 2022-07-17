eaa6cf0290494675ec5a3474745471ca30b28d04
chenjiajian <770230504@qq.com>
2022-07-17 18:10:19 +0800

    暂存

---
+++ src/list/demo/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <tabs v-model="active" line-animation tab-animation>
+    <tab title="基础用法">
+      <List></List>
+    </tab>
+    <tab title="错误提示">
+      <List></List>
+    </tab>
+    <tab title="下拉刷新">
+      <PullRefresh @refresh="refresh">
+        <List></List>
+      </PullRefresh>
+    </tab>
+  </tabs>
+</template>
+
+<script setup>
+import { ref } from '@vue/reactivity'
+
+import list from '../index.vue'
+import tab from '../../tab/index.vue'
+import tabs from '../../tabs/index.vue'
+import pullRefresh from '../../pull-refresh/index.vue'
+import PullRefresh from '../../pull-refresh/index.vue'
+import List from '../index.vue'
+const active = ref(0)
+
+const refresh = done => {
+  setTimeout(done, 2000)
+}
+</script>
+
+<style lang="less">
+.demo-list {
+  background: #fff;
+  .tabs-body {
+    padding: 0;
+  }
+}
+</style>
---
+++ src/list/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">list组件</div>
+</template>
+
+<script>
+const name = 'list'
+
+function setup(props) {}
+export default {
+  name,
+  setup
+}
+</script>
+
+<style lang="less" scoped>
+.list {
+  // min-height: 100vh;
+  width: 100%;
+  background: #fff;
+}
+</style>
