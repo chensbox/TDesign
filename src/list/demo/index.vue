536776917083fecf0ceec72e524ddcded4a06ea3
chenjiajian <770230504@qq.com>
2022-07-19 20:07:18 +0800

    demo

--- src/list/demo/index.vue
+++ src/list/demo/index.vue
@@ -1,21 +1,16 @@
 <template>
   <tabs v-model="active" line-animation tab-animation>
     <tab title="基础用法">
-      <list
-        v-model:loading="loading"
-        v-model:error="error"
-        :finished="finished"
-        @load="onLoad"
-      >
+      <list v-model:loading="loading" :finished="finished" @load="onLoad">
         <div class="cell" v-for="item in listdata" :key="item">{{ item }}</div>
       </list>
     </tab>
     <tab title="错误提示">
       <list
-        v-model:loading="loading"
+        v-model:loading="err_loading"
         v-model:error="error"
-        :finished="finished"
-        @load="onLoad"
+        :finished="err_finished"
+        @load="err_onLoad"
       >
         <div class="cell" v-for="item in err_listdata" :key="item">
           {{ item }}
@@ -25,10 +20,9 @@
     <tab title="下拉刷新">
       <pull-refresh @refresh="refresh">
         <list
-          v-model:loading="loading"
-          v-model:error="error"
-          :finished="finished"
-          @load="onLoad"
+          v-model:loading="pull_loading"
+          :finished="pull_finished"
+          @load="pull_onLoad"
         >
           <div class="cell" v-for="item in pull_listdata" :key="item">
             {{ item }}
@@ -45,11 +39,19 @@ import list from '../index.vue'
 import tab from '../../tab/index.vue'
 import tabs from '../../tabs/index.vue'
 import pullRefresh from '../../pull-refresh/index.vue'
+import { watch } from '@vue/runtime-core'
 
 const active = ref(0)
+
 const loading = ref(false)
+const err_loading = ref(true)
+const pull_loading = ref(true)
+
 const error = ref(false)
+
 const finished = ref(false)
+const err_finished = ref(false)
+const pull_finished = ref(false)
 
 const listdata = ref([])
 const err_listdata = ref([])
@@ -69,6 +71,36 @@ const onLoad = () => {
 
     if (listdata.value.length >= 40) {
       finished.value = true
+      return
+    }
+  }, 1500)
+}
+
+const err_onLoad = () => {
+  err_loading.value = true
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      err_listdata.value.push(err_listdata.value.length + 1)
+    }
+    err_loading.value = false
+
+    if (err_listdata.value.length >= 40) {
+      err_finished.value = true
+      return
+    }
+  }, 1500)
+}
+
+const pull_onLoad = () => {
+  pull_loading.value = true
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      pull_listdata.value.push(pull_listdata.value.length + 1)
+    }
+    pull_loading.value = false
+
+    if (pull_listdata.value.length >= 40) {
+      pull_finished.value = true
       return
     }
   }, 1500)
